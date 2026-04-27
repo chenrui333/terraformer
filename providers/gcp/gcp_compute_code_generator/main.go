70ac264d8312e7870637ed6f892ec949cf922fac
Rui Chen <rui@chenrui.dev>
2026-04-27 11:37:58 -0400

    chore(license): Replace Apache 2.0 license boilerplate with SPDX-License-Identifier (#184)
    * Replace Apache 2.0 license boilerplate with SPDX-License-Identifier
    
    Replace the 13-line Apache 2.0 license header in all Go source files
    with the concise SPDX-License-Identifier tag.
    
    Closes #183
    
    * Add License section to README with SPDX identifier

--- providers/gcp/gcp_compute_code_generator/main.go
+++ providers/gcp/gcp_compute_code_generator/main.go
@@ -1,16 +1,4 @@
-// Copyright 2018 The Terraformer Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 //nolint:gosec,staticcheck // lint triage: legacy provider/API/security baseline is tracked in #175.
 package main
@@ -27,19 +15,7 @@ import (
 
 const pathForGenerateFiles = "/providers/gcp/"
 const serviceTemplate = `
-// Copyright 2018 The Terraformer Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 // AUTO-GENERATED CODE. DO NOT EDIT.
 package gcp
@@ -121,19 +97,7 @@ func (g *{{.titleResourceName}}Generator) InitResources() error {
 
 `
 const computeTemplate = `
-// Copyright 2018 The Terraformer Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 // AUTO-GENERATED CODE. DO NOT EDIT.
 package gcp
