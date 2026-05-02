51915ae5af8ae21f85489262cf00a7bbd398df23
Rui Chen <rui@chenrui.dev>
2026-05-02 17:47:30 -0400

    test(datadog): cover omitted synthetics variable ids
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- providers/datadog/synthetics_global_variable_test.go
+++ providers/datadog/synthetics_global_variable_test.go
@@ -16,7 +16,9 @@ import (
 func TestSyntheticsGlobalVariableInitResourcesReturnsFilteredIDError(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/api/v1/synthetics/variables/global-1" {
-			t.Fatalf("unexpected path %s", r.URL.Path)
+			t.Errorf("unexpected path %s", r.URL.Path)
+			http.NotFound(w, r)
+			return
 		}
 		http.Error(w, "{\"errors\":[\"service unavailable\"]}", http.StatusServiceUnavailable)
 	}))
@@ -50,5 +52,42 @@ func TestSyntheticsGlobalVariableInitResourcesReturnsFilteredIDError(t *testing.
 	}
 	if !strings.Contains(err.Error(), "get datadog synthetics global variable global-1") {
 		t.Fatalf("InitResources error = %q, want wrapped global variable error", err)
+	}
+}
+
+func TestSyntheticsGlobalVariableInitResourcesOmitsFilteredIDSucceeds(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Errorf("unexpected request for omitted synthetics global variable: %s", r.URL.Path)
+		http.NotFound(w, r)
+	}))
+	t.Cleanup(server.Close)
+
+	config := datadog.NewConfiguration()
+	config.Servers = datadog.ServerConfigurations{{URL: server.URL}}
+	config.HTTPClient = server.Client()
+
+	generator := &SyntheticsGlobalVariableGenerator{
+		DatadogService: DatadogService{
+			Service: terraformutils.Service{
+				Args: map[string]interface{}{
+					"auth":          context.Background(),
+					"datadogClient": datadog.NewAPIClient(config),
+				},
+				Filter: []terraformutils.ResourceFilter{
+					{
+						ServiceName:      "synthetics_global_variable",
+						FieldPath:        "id",
+						AcceptableValues: nil,
+					},
+				},
+			},
+		},
+	}
+
+	if err := generator.InitResources(); err != nil {
+		t.Fatalf("InitResources returned error for omitted synthetics global variable ID: %v", err)
+	}
+	if len(generator.Resources) != 0 {
+		t.Fatalf("Resources length = %d, want 0", len(generator.Resources))
 	}
 }
