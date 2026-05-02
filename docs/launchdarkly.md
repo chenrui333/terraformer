30495c89d8e9bc551467433ff74c66836fc445da
Rui Chen <rui@chenrui.dev>
2026-05-01 20:31:05 -0400

    feat(launchdarkly): add environment resource support
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- docs/launchdarkly.md
+++ docs/launchdarkly.md
@@ -4,13 +4,15 @@ Example:
 
 ```
 export LAUNCHDARKLY_ACCESS_TOKEN=[LAUNCHDARKLY_ACCESS_TOKEN]
-./terraformer import launchdarkly -r project,featureFlag,segment
+./terraformer import launchdarkly -r project,environment,featureFlag,segment
 ```
 
 List of supported LaunchDarkly resources:
 
 *   `project`
     * `launchdarkly_project`
+*   `environment`
+    * `launchdarkly_environment`
 *   `featureFlag`
     * `launchdarkly_feature_flag`
     * `launchdarkly_feature_flag_environment`
---
+++ providers/launchdarkly/environment.go
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package launchdarkly
+
+import (
+	"context"
+
+	"github.com/chenrui333/terraformer/terraformutils"
+	ldapi "github.com/launchdarkly/api-client-go/v16"
+)
+
+type EnvironmentGenerator struct {
+	LaunchDarklyService
+}
+
+func (g *EnvironmentGenerator) loadEnvironments(ctx context.Context, client *ldapi.APIClient, projectKey string) error {
+	envs, err := getEnvironments(ctx, client, projectKey)
+	if err != nil {
+		return err
+	}
+	for _, env := range envs {
+		resource := terraformutils.NewResource(
+			projectKey+"/"+env.Key,
+			projectKey+"-"+env.Name,
+			"launchdarkly_environment",
+			"launchdarkly",
+			map[string]string{
+				"key":         env.Key,
+				"project_key": projectKey,
+			},
+			[]string{},
+			map[string]interface{}{})
+		g.Resources = append(g.Resources, resource)
+	}
+	return nil
+}
+
+func (g *EnvironmentGenerator) InitResources() error {
+	ctx := g.GetArgs()["ctx"].(context.Context)
+	client := g.GetArgs()["client"].(*ldapi.APIClient)
+
+	projects, err := getProjects(ctx, client)
+	if err != nil {
+		return err
+	}
+	for _, project := range projects {
+		if err := g.loadEnvironments(ctx, client, project.Key); err != nil {
+			return err
+		}
+	}
+	return nil
+}
