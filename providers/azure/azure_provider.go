14c23e1f732a4b98cf0f4b316e3d5e3502f4af36
Rui Chen <rui@chenrui.dev>
2026-05-03 09:25:33 -0400

    fix(azure): stage init state until success
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- providers/azure/azure_provider.go
+++ providers/azure/azure_provider.go
@@ -539,21 +539,24 @@ func (p *AzureProvider) Init(args []string) error {
 		return errors.New("azure: expected 1 init arg (resource group)")
 	}
 
-	err := p.setEnvConfig()
+	staged := &AzureProvider{}
+	err := staged.setEnvConfig()
 	if err != nil {
 		return err
 	}
 
-	clientOptions, err := p.getClientOptions()
+	clientOptions, err := staged.getClientOptions()
 	if err != nil {
 		return err
 	}
+	staged.clientOptions = clientOptions
+
+	credential, err := staged.getTokenCredential()
+	if err != nil {
+		return err
+	}
+	p.config = staged.config
 	p.clientOptions = clientOptions
-
-	credential, err := p.getTokenCredential()
-	if err != nil {
-		return err
-	}
 	p.credential = credential
 	p.resourceGroup = args[0]
 
